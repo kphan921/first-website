a55cf4f82946eaf8059eefe520db0dcb07788885
Tri <triphan921@gmail.com>
2021-02-20 18:11:11 -0800

    animate gradient background, click events

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// const alert = document.getElementsByClassName('button2');
+const button1 = document.getElementById('button1');
+const body = document.querySelector('body');
 
-// function clickAlert() {
-//     alert('Click click!');
-// }
+button1.addEventListener('click', changeBackground);
 
-// alert.addEventListener('click', clickAlert)
+// setTimeout(() => {
+//     button1.removeEventListener('click', changeBackground)
+// }, 2000)
 
-function addingEventListener() {
-    const input = document.getElementsByClassName('button2');
-
-    function clickAlert() {
-        alert('Click!');
-    }
-
-    input.addEventListener('click', clickAlert); 
+function changeBackground() {
+    body.style.background = "linear-gradient(to right, white, red)";
 }
 
-input.addEventListener('click', addingEventListener);
+
+const button2 = document.getElementById('button2');
+
+button2.addEventListener('click', e => { alert('Click! Click!')});
