0915e83ffbbd27e0d4f5d56de39fd6194698e191
Tri <triphan921@gmail.com>
2021-02-28 20:43:55 -0800

    add car form

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+const cars = [
+  {
+    imgUrl: 'http://jdm-expo.com/17353/nissan-skyline-sale.jpg',
+    make: 'Nissan Skyline',
+    model: 'Skyline',
+    year: '1999',
+    cost: '$100,000'
+  },
+  {
+    imgUrl: 'https://www.meme-arsenal.com/memes/0ca89633a5a07eee8a3e71fa416bfb82.jpg',
+    make: 'Toyota Supra',
+    model: 'Supra',
+    year: '1994',
+    cost: '$60,000'
+  },
+  {
+    imgUrl: 'https://cdn.dealeraccelerate.com/international/1/10/4982/790x1024/1991-honda-nsx',
+    make: 'Honda NSX',
+    model: 'NSX',
+    year: '1999',
+    cost: '$100,000'
+  },
+  {
+    imgUrl: 'https://i.kinja-img.com/gawker-media/image/upload/s--14kWoCVg--/c_scale,f_auto,fl_progressive,q_80,w_800/clrkfmgms31kvu757dl3.jpg',
+    make: 'Mazda RX7',
+    model: 'RX7',
+    year: '1995',
+    cost: '$30,000'
+  }
+]
+
+const buildCars = () => {
+  cars.forEach(car => buildCar(car));
+};
+const buildCar = (car) => {
+  let carList = document.querySelector('.car-list');
+  let carName = document.createElement('h3');
+  carName.textContent = car.make;
+  carList.appendChild(carName);
+  carName.addEventListener('click', () => showCar(car))
+};
+buildCars();
+
+const showCar = (car) => {
+  let carList = document.querySelector('.car-list');
+  let image = document.createElement('img');
+  let carName = document.createElement('h3');
+  let carModel = document.createElement('h4');
+  let carYear = document.createElement('h4');
+  let carCost = document.createElement('h4');
+
+  carList.classList = 'car';
+  image.src = car.imgUrl;
+  carName.textContent = car.make;
+  carModel.textContent = car.model;
+  carYear.textContent = car.year;
+  carCost.textContent = car.cost;
+  carList.append(image, carName, carModel, carYear,carCost);
+}
+
+
+
+
+
 const button1 = document.getElementById('button1');
 const body = document.querySelector('body');
 
